43fffec220602267ad9ef43ff45ac8e11e7e9625
mohamedNaser1212 <nasergamal33e@gmail.com>
2023-07-25 19:12:52 +0300

    refactor initstate and navigate home

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const KPrimaryColor =  Color(0xff100B20);
+const KTransitionDuration = Duration(milliseconds: 300);
--- lib/views/widgets/splash_view_body.dart
+++ lib/views/widgets/splash_view_body.dart
@@ -1,6 +1,12 @@
 import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/views/widgets/slidingtext.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/src/routes/transitions_type.dart';
+
+import '../../constants.dart';
+import '../../features/home/data/presentetion/views/home_view.dart';
 
 class splashViewbody extends StatefulWidget {
   const splashViewbody({super.key});
@@ -18,13 +24,14 @@ class _splashViewbodyState extends State<splashViewbody>
   @override
   void initState() {
     super.initState();
-    animationController =
-        AnimationController(vsync: this, duration: const Duration(seconds: 2));
-    slidingAnimation=Tween<Offset>(begin:const Offset(0,10) ,end:Offset.zero).animate(animationController);
-
-    animationController.forward();
+    initslidinganimation();
+    navigateToHome();
 
   }
+
+
+
+
 @override
   void dispose() {
 
@@ -44,6 +51,19 @@ class _splashViewbodyState extends State<splashViewbody>
      slidingText(slidingAnimation: slidingAnimation,),
       ],
     );
+
+  }
+  void initslidinganimation() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    slidingAnimation=Tween<Offset>(begin:const Offset(0,10) ,end:Offset.zero).animate(animationController);
+
+    animationController.forward();
+  }
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 2),(){
+      Get.to(()=>const HomeView(),transition:Transition.fade,duration:KTransitionDuration);
+    });
   }
 }
 

53158500a94b8a331619c77140fd63447eefa50e
mohamedNaser1212 <nasergamal33e@gmail.com>
2023-07-26 12:07:15 +0300

    best seller list view item added

--- lib/views/widgets/home_view_body.dart
+++ lib/views/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 
+import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -17,14 +18,51 @@ class HomeViewBody extends StatelessWidget {
         children: [
           CustomAppBar(),
           FeaturedBookListview(),
-          const SizedBox(
+          SizedBox(
             height: 50,
           ),
           Text('Best Seller',style: Styles.titleMedium),
+          BestSellerListViewItem(),
         ],
       ),
     );
   }
 }
 
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+        AspectRatio(
+        aspectRatio: 2.5 / 4, //raatio of the width with respect to height
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: Colors.red,
+            image: const DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage(
+                AssetsData.test_image,
+              ),
+            ),
+          ),
+        ),
+      ),
+          const Column(
+            children: [
+              Text(''),
+
+            ],
+          ),
+
+        ],
+      ),
+    );
+  }
+}
 

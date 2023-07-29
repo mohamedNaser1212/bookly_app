8071c5523352c9f45a67e6ce546c7a68597529fa
mohamedNaser1212 <nasergamal33e@gmail.com>
2023-07-29 16:29:50 +0300

    best seller item done

--- lib/views/widgets/best_seller_list_view.dart
+++ lib/views/widgets/best_seller_list_view.dart
@@ -1,9 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../../constants.dart';
 import '../../core/utils/assets.dart';
 import '../../core/utils/styles.dart';
+import 'book_rating_.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
   const BestSellerListViewItem({super.key});
@@ -32,45 +32,49 @@ class BestSellerListViewItem extends StatelessWidget {
           const SizedBox(
             width: 30,
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width:MediaQuery.of(context).size.width*0.5,
-                // height: 120,
-                child:  Text(
-                  'Harry Potter and the Goblet of Fire',
-                  style: Styles.textStyle20.copyWith(
-                      fontFamily:kGtSectraFine
+          //Expanded because the column is the parent of the row below i tell it to expand so the row also expands
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  // height: 120,
+                  child: Text(
+                    'Harry Potter and the Goblet of Fire',
+                    style:
+                        Styles.textStyle20.copyWith(fontFamily: kGtSectraFine),
+                    maxLines: 2,
+                    overflow: TextOverflow
+                        .ellipsis, //dots when the text take long space
                   ),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis, //dots when the text take long space
                 ),
-
-              ),
-              const SizedBox(
-                height: 3,
-              ),
-              const Text(
-                'J.K. Rowling',
-                style: Styles.textStyle14
-                //dots when the text take long space
-              ),
-              const SizedBox(
-                width: 3,
-              ),
-              Row(
-                children: [
-                  Text('19,99 € ',style: Styles.textStyle20.copyWith(
-                      fontWeight: FontWeight.bold
-                  ),)
-
-                ],
-              )
-            ],
+                const SizedBox(
+                  height: 3,
+                ),
+                const Text('J.K. Rowling', style: Styles.textStyle14
+                    //dots when the text take long space
+                    ),
+                const SizedBox(
+                  width: 3,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      '19,99 € ',
+                      style: Styles.textStyle20
+                          .copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    const Spacer(),
+                    const BookRating(),
+                  ],
+                )
+              ],
+            ),
           ),
         ],
       ),
     );
   }
 }
+

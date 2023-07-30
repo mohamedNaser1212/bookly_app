2ce0845dd04daf9c1bd435bb822b7844db0e6dcb
mohamedNaser1212 <nasergamal33e@gmail.com>
2023-07-30 12:54:25 +0300

    add rating to book details

--- lib/features/home/data/presentetion/views/book_details_view_body.dart
+++ lib/features/home/data/presentetion/views/book_details_view_body.dart
@@ -1,46 +1,54 @@
 import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/views/widgets/CustomBookImageItem.dart';
+import 'package:bookly/views/widgets/book_rating_.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../../views/widgets/custom_book_details_appbar.dart';
 
-class BookDetailsViewBody extends  StatelessWidget {
+class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var width=MediaQuery.of(context).size.width;
-    return    Padding(
+    var width = MediaQuery.of(context).size.width;
+    return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30.0),
       child: Column(
         children: [
           const CustomBookDetailsAppBar(),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: width*0.2),
-            child:const CustomBookImage(),
+            padding: EdgeInsets.symmetric(horizontal: width * 0.2),
+            child: const CustomBookImage(),
           ),
           const SizedBox(
             height: 43,
           ),
-           Text('The Jungle Book',style: Styles.textStyle30.copyWith(
-            fontWeight: FontWeight.bold,
-
-          ),),
+          Text(
+            'The Jungle Book',
+            style: Styles.textStyle30.copyWith(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
           const SizedBox(
             height: 6,
           ),
-           Opacity(
-             opacity: 0.7,
-             child: Text('Rudyard Kipling',style: Styles.textStyle18.copyWith(
-              fontStyle: FontStyle.italic,
-               
-          ),),
-           ),
-
+          Opacity(
+            opacity: 0.7,
+            child: Text(
+              'Rudyard Kipling',
+              style: Styles.textStyle18.copyWith(
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 18,
+          ),
+          const BookRating(
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
         ],
-
       ),
     );
   }
 }
-
--- lib/views/widgets/book_rating_.dart
+++ lib/views/widgets/book_rating_.dart
@@ -4,32 +4,36 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../../core/utils/styles.dart';
 
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
-
+  const BookRating({super.key,  this.mainAxisAlignment=MainAxisAlignment.start});
+final MainAxisAlignment mainAxisAlignment;
   @override
   Widget build(BuildContext context) {
     return  Row(
-      children: [
+      mainAxisAlignment: mainAxisAlignment,
+      children:  [
         const Icon(
           FontAwesomeIcons.solidStar,
+          size: 14,
           color: Color(0xffFFDD4F),
         ),
-        const SizedBox(
+        const  SizedBox(
           width: 6.3,
         ),
-        const Text(
+        const    Text(
           '4.8',
           style: Styles.textStyle16,
         ),
-        const SizedBox(
+        const    SizedBox(
           width: 5,
         ),
-        Text(
-          '(2390)',
-          style: Styles.textStyle14.copyWith(
-            color: const Color(0xff707070)
+        Opacity(
+          opacity: 0.5,
+          child: Text(
+            '(2390)',
+            style: Styles.textStyle14.copyWith(
+              fontWeight: FontWeight.w600
+            )
           ),
-
         )
       ],
     );

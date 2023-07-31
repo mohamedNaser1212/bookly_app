7e3c9f6a7e1929212ddf8c8b345a71ea5436319a
mohamedNaser1212 <nasergamal33e@gmail.com>
2023-07-31 19:58:02 +0300

    Search result view ui done

--- lib/features/home/data/presentetion/views/widgets/SearchViewBody.dart
+++ lib/features/home/data/presentetion/views/widgets/SearchViewBody.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../../core/utils/styles.dart';
+import '../../../../../../views/widgets/best_seller_list_view_item.dart';
 import '../../../../../../views/widgets/custom_search_text_field.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -10,9 +12,35 @@ class SearchViewBody extends StatelessWidget {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 30.0),
       child: Column(
-        children: [CustomSearchTextField()],
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomSearchTextField(),
+          SizedBox(
+            height: 16,
+          ),
+          Text('Search Result', style: Styles.textStyle18),
+          Expanded(child: SearchResultListView()),
+        
+        ],
       ),
     );
   }
 }
 
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      // shrinkWrap: true,//to solve the problem of the sliver //it removed because we add Sliverfillremaining
+        padding: EdgeInsets.zero,
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(vertical: 10.0),
+            child: BestSellerListViewItem(),
+          );
+        });
+  }
+}

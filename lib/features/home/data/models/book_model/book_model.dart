6758013652fff8616ffa423200c11f149c5abf00
mohamedNaser1212 <nasergamal33e@gmail.com>
2023-08-01 18:44:56 +0300

    create home repo

--- lib/features/home/data/models/book_model/book_model.dart
+++ lib/features/home/data/models/book_model/book_model.dart
@@ -10,7 +10,7 @@ class BookModel extends Equatable {
   final String? id;
   final String? etag;
   final String? selfLink;
-  final VolumeInfo volumeInfo;
+  final VolumeInfo? volumeInfo;
   final SaleInfo? saleInfo;
   final AccessInfo? accessInfo;
   final SearchInfo? searchInfo;
@@ -49,7 +49,7 @@ class BookModel extends Equatable {
         'id': id,
         'etag': etag,
         'selfLink': selfLink,
-        'volumeInfo': volumeInfo.toJson(),
+        'volumeInfo': volumeInfo?.toJson(),
         'saleInfo': saleInfo?.toJson(),
         'accessInfo': accessInfo?.toJson(),
         'searchInfo': searchInfo?.toJson(),

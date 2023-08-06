3f17b4b8c3bb710cd238ab7d1962795c8bfe3821
mohamedNaser1212 <nasergamal33e@gmail.com>
2023-08-06 17:53:42 +0300

    implement to fetchnewsetbooks

--- lib/features/home/data/repos/home_repo_implementation.dart
+++ lib/features/home/data/repos/home_repo_implementation.dart
@@ -1,4 +1,5 @@
 import 'package:bookly/core/errors/failures.dart';
+import 'package:bookly/core/utils/api_service.dart';
 import 'package:dartz/dartz.dart';
 
 
@@ -6,11 +7,24 @@ import '../models/book_model/book_model.dart';
 import 'home_repo.dart';
 
 class HomeRepoImp1 implements HomeRepo{
+  final ApiService apiService;
+
+  HomeRepoImp1(this.apiService);
   @override
-  Future<Either<Failure ,List<BookModel>>> fetchBestSellerBooks(){
+  Future<Either<Failure ,List<BookModel>>> fetchNewsetBooks() async{
 
+try {
+  var data= await apiService.get(endPoint: 'volumes?Filtering=free-ebooks&Sorting=newest &q=subject:Programing');
+List <BookModel>books=[];
+for(var item in data['items']){
+  books.add(BookModel.fromJson(item));
+}
+return right(books);
 
-    throw UnimplementedError();
+}  catch (e) {
+return left(ServerFailure());
+}
+
   }
   @override
   Future<Either<Failure ,List<BookModel>>> fetchFeaturedBooks(){
